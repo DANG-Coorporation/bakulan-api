39b6d418389f42d35ff18aea1ac0cebf4999afcb
Galih Setyawan <galih.kamvax032@gmail.com>
2023-10-16 19:48:27 +0700

    fix: model product categories

---
+++ src/database/migrations/20231016124627-AlterTableCategoriesAddProductid.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn("categories", "product_id", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: {
+          tableName: "products",
+        },
+        key: "id",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("categories", "product_id");
+  },
+};
